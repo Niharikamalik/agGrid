d02515b0f81cf7027460108aeb05796c5a009645
Niharika Malik <73269292+Niharikamalik@users.noreply.github.com>
2024-03-04 12:13:47 +0530

    Add files via upload

--- agGrid/src/app/app.module.ts
+++ agGrid/src/app/app.module.ts
@@ -4,16 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AgGridAngular } from 'ag-grid-angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgGridComponent } from './pages/ag-grid/ag-grid.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
- 
+    AgGridComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AgGridAngular
+    AgGridAngular,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
